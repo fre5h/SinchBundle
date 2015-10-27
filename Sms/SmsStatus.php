6723e2704cf02842d4f4d5a1c8f40510ef4b3dfd
Artem Genvald <genvaldartem@gmail.com>
2015-10-27 21:32:39 +0200

    Add sms statuses

---
+++ Sms/SmsStatus.php
@@ -0,0 +1,24 @@
+<?php
+/*
+ * This file is part of the FreshSinchBundle
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\SinchBundle\Sms;
+
+/**
+ * Sinch SMS Status
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+final class SmsStatus
+{
+    const PENDING    = 'pending';
+    const SUCCESSFUL = 'successful';
+    const FAULTED    = 'faulted';
+    const UNKNOWN    = 'unknown';
+}
