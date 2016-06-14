201e9c144f7fba62532b99d854e0fd21c66d38b3
Artem Genvald <genvaldartem@gmail.com>
2016-06-14 13:00:54 +0300

    Add list of supported by Sinch countries

---
+++ Helper/SinchSupportedCountries.php
@@ -0,0 +1,145 @@
+<?php
+/*
+ * This file is part of the FreshSinchBundle
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\SinchBundle\Helper;
+
+/**
+ * SinchSupportedCountries.
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+class SinchSupportedCountries
+{
+    /**
+     * List of supported countries by Sinch.
+     *
+     * @var array $supportedCountries Countries
+     * @static
+     */
+    public static $supportedCountries = [
+        'AF' => 'Afghanistan',
+        'AM' => 'Armenia',
+        'AR' => 'Argentina',
+        'AT' => 'Austria',
+        'AU' => 'Australia',
+        'AZ' => 'Azerbaijan',
+        'BA' => 'Bosnia & Herzegovina',
+        'BD' => 'Bangladesh',
+        'BE' => 'Belgium',
+        'BG' => 'Bulgaria',
+        'BH' => 'Bahrain',
+        'BO' => 'Bolivia',
+        'BR' => 'Brazil',
+        'BS' => 'Bahamas',
+        'BY' => 'Belarus',
+        'CA' => 'Canada',
+        'CD' => 'Congo - Kinshasa',
+        'CF' => 'Central African Republic',
+        'CG' => 'Congo - Brazzaville',
+        'CH' => 'Switzerland',
+        'CL' => 'Chile',
+        'CN' => 'China',
+        'CO' => 'Colombia',
+        'CR' => 'Costa Rica',
+        'CU' => 'Cuba',
+        'CY' => 'Cyprus',
+        'CZ' => 'Czech Republic',
+        'DE' => 'Germany',
+        'DK' => 'Denmark',
+        'DZ' => 'Algeria',
+        'EC' => 'Ecuador',
+        'EG' => 'Egypt',
+        'ES' => 'Spain',
+        'FI' => 'Finland',
+        'FR' => 'France',
+        'GA' => 'Gabon',
+        'GB' => 'United Kingdom',
+        'GE' => 'Georgia',
+        'GH' => 'Ghana',
+        'GM' => 'Gambia',
+        'GR' => 'Greece',
+        'HK' => 'Hong Kong SAR China',
+        'HR' => 'Croatia',
+        'HU' => 'Hungary',
+        'ID' => 'Indonesia',
+        'IE' => 'Ireland',
+        'IL' => 'Israel',
+        'IN' => 'India',
+        'IR' => 'Iran',
+        'IT' => 'Italy',
+        'JM' => 'Jamaica',
+        'JP' => 'Japan',
+        'KE' => 'Kenya',
+        'KG' => 'Kyrgyzstan',
+        'KH' => 'Cambodia',
+        'KR' => 'South Korea',
+        'KW' => 'Kuwait',
+        'KZ' => 'Kazakhstan',
+        'LB' => 'Lebanon',
+        'LR' => 'Liberia',
+        'LT' => 'Lithuania',
+        'LU' => 'Luxembourg',
+        'LV' => 'Latvia',
+        'LY' => 'Libya',
+        'MA' => 'Morocco',
+        'MC' => 'Monaco',
+        'MD' => 'Moldova',
+        'MK' => 'Macedonia',
+        'MN' => 'Mongolia',
+        'MX' => 'Mexico',
+        'MY' => 'Malaysia',
+        'NG' => 'Nigeria',
+        'NL' => 'Netherlands',
+        'NO' => 'Norway',
+        'NZ' => 'New Zealand',
+        'PA' => 'Panama',
+        'PE' => 'Peru',
+        'PH' => 'Philippines',
+        'PK' => 'Pakistan',
+        'PL' => 'Poland',
+        'PR' => 'Puerto Rico',
+        'PT' => 'Portugal',
+        'PY' => 'Paraguay',
+        'QA' => 'Qatar',
+        'RO' => 'Romania',
+        'RU' => 'Russia',
+        'SA' => 'Saudi Arabia',
+        'SD' => 'Sudan',
+        'SE' => 'Sweden',
+        'SG' => 'Singapore',
+        'SI' => 'Slovenia',
+        'SK' => 'Slovakia',
+        'SN' => 'Senegal',
+        'SY' => 'Syria',
+        'TH' => 'Thailand',
+        'TN' => 'Tunisia',
+        'TR' => 'Turkey',
+        'TW' => 'Taiwan',
+        'UA' => 'Ukraine',
+        'UG' => 'Uganda',
+        'US' => 'United States',
+        'UY' => 'Uruguay',
+        'VE' => 'Venezuela',
+        'VN' => 'Vietnam',
+        'ZA' => 'South Africa',
+    ];
+
+    /**
+     * Check if country is supported
+     *
+     * @param string $countryCode Country code
+     *
+     * @return bool
+     */
+    public static function isCountrySupported($countryCode)
+    {
+        return isset(self::$supportedCountries[$countryCode]);
+    }
+}
---
+++ Tests/Helper/SinchSupportedCountriesTest.php
@@ -0,0 +1,31 @@
+<?php
+/*
+ * This file is part of the FreshSinchBundle
+ *
+ * (c) Artem Genvald <genvaldartem@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Fresh\SinchBundle\Tests\Helper;
+
+use Fresh\SinchBundle\Helper\SinchSupportedCountries;
+
+/**
+ * SinchSupportedCountriesTest.
+ *
+ * @author Artem Genvald <genvaldartem@gmail.com>
+ */
+class SinchSupportedCountriesTest extends \PHPUnit_Framework_TestCase
+{
+    public function testSupportedCountry()
+    {
+        $this->assertTrue(SinchSupportedCountries::isCountrySupported('UA'));
+    }
+
+    public function testUnsupportedCountry()
+    {
+        $this->assertFalse(SinchSupportedCountries::isCountrySupported('AQ'));
+    }
+}
